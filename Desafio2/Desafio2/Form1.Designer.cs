93e19d49794ade67e43d91a84e6491b9637e5a09
Carlos <71299504+NoobMaster-69-UwU@users.noreply.github.com>
2024-05-25 21:25:40 -0600

    Version 1
    Mapa de El Salvador - Funcional

---
+++ Desafio2/Desafio2/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace Desafio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBoxFin = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBoxInicio = new System.Windows.Forms.ComboBox();
+            this.pictureBoxGrafo = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGrafo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Beige;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.comboBoxFin);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBoxInicio);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(61, 612);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1111, 148);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Funciones";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(856, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(183, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Ruta más Corta";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBoxFin
+            // 
+            this.comboBoxFin.FormattingEnabled = true;
+            this.comboBoxFin.Location = new System.Drawing.Point(253, 75);
+            this.comboBoxFin.Name = "comboBoxFin";
+            this.comboBoxFin.Size = new System.Drawing.Size(137, 28);
+            this.comboBoxFin.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(650, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Profundidad";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(468, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Anchura";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBoxInicio
+            // 
+            this.comboBoxInicio.FormattingEnabled = true;
+            this.comboBoxInicio.Location = new System.Drawing.Point(56, 75);
+            this.comboBoxInicio.Name = "comboBoxInicio";
+            this.comboBoxInicio.Size = new System.Drawing.Size(142, 28);
+            this.comboBoxInicio.TabIndex = 0;
+            // 
+            // pictureBoxGrafo
+            // 
+            this.pictureBoxGrafo.BackColor = System.Drawing.Color.Linen;
+            this.pictureBoxGrafo.Location = new System.Drawing.Point(61, 57);
+            this.pictureBoxGrafo.Name = "pictureBoxGrafo";
+            this.pictureBoxGrafo.Size = new System.Drawing.Size(1111, 549);
+            this.pictureBoxGrafo.TabIndex = 1;
+            this.pictureBoxGrafo.TabStop = false;
+            this.pictureBoxGrafo.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBoxGrafo.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxGrafo_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Punto Inicio";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(249, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Punto Fin";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(1223, 352);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 108);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Volver al Menú";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(1383, 772);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBoxGrafo);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Ruta El Salvador";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGrafo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBoxGrafo;
+        private System.Windows.Forms.ComboBox comboBoxInicio;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBoxFin;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Desafio2/Desafio2/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio2
+{
+
+
+    public partial class Form1 : Form
+    {
+        private Grafo grafo;
+        public Form1()
+        {
+            InitializeComponent();
+      
+            InicializarGrafo();
+            LlenarComboBoxes();
+            DibujarGrafo(null);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inicio = comboBoxInicio.SelectedItem.ToString();
+            string fin = comboBoxFin.SelectedItem.ToString();
+            List<string> recorrido = grafo.Anchura(inicio, fin);
+            MessageBox.Show($"Ruta más corta por Anchura: {string.Join(" -> ", recorrido)}");
+            DibujarGrafo(recorrido);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string inicio = comboBoxInicio.SelectedItem.ToString();
+            string fin = comboBoxFin.SelectedItem.ToString();
+            List<string> recorrido = grafo.Profundidad(inicio, fin);
+            MessageBox.Show($"Ruta más corta por Profundidad: {string.Join(" -> ", recorrido)}");
+            DibujarGrafo(recorrido);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            string inicio = comboBoxInicio.SelectedItem.ToString();
+            string fin = comboBoxFin.SelectedItem.ToString();
+            var camino = grafo.Dijkstra(inicio, fin, out int distanciaTotal);
+            MessageBox.Show($"Camino más corto de {inicio} a {fin} (Dijkstra): {string.Join(" -> ", camino)}\nDistancia total: {distanciaTotal}");
+            DibujarGrafo(camino);
+        }
+
+        private void pictureBoxGrafo_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        private void InicializarGrafo()
+        {
+            grafo = new Grafo();
+
+            grafo.AgregarVertice("Ahuachapan");
+            grafo.AgregarVertice("Santa Ana");
+            grafo.AgregarVertice("Sonsonate");
+            grafo.AgregarVertice("Chalatenango");
+            grafo.AgregarVertice("Cuscatlan");
+            grafo.AgregarVertice("San Salvador");
+            grafo.AgregarVertice("Cabañas");
+            grafo.AgregarVertice("San Vicente");
+            grafo.AgregarVertice("La Paz");
+            grafo.AgregarVertice("La Libertad");
+            grafo.AgregarVertice("Usulutan");
+            grafo.AgregarVertice("San Miguel");
+            grafo.AgregarVertice("Morazan");
+            grafo.AgregarVertice("La Union");
+
+         
+            grafo.AgregarArista("Ahuachapan", "Santa Ana", 83);
+            grafo.AgregarArista("Ahuachapan", "Sonsonate", 56);
+            grafo.AgregarArista("Santa Ana", "Chalatenango", 53);
+            grafo.AgregarArista("Santa Ana", "La Libertad", 45);
+            grafo.AgregarArista("Santa Ana", "Sonsonate", 76);
+            grafo.AgregarArista("Sonsonate", "La Libertad", 38);
+            grafo.AgregarArista("Chalatenango", "La Libertad", 67);
+            grafo.AgregarArista("Chalatenango", "Cuscatlan", 76);
+            grafo.AgregarArista("Chalatenango", "San Salvador", 74);
+            grafo.AgregarArista("Chalatenango", "Cabañas", 67);
+            grafo.AgregarArista("Cuscatlan", "San Vicente", 49);
+            grafo.AgregarArista("Cuscatlan", "San Salvador", 55);
+            grafo.AgregarArista("Cuscatlan", "Cabañas", 56);
+            grafo.AgregarArista("Cuscatlan", "La Paz", 88);
+            grafo.AgregarArista("San Salvador", "La Libertad", 64);
+            grafo.AgregarArista("San Salvador", "La Paz", 78);
+            grafo.AgregarArista("San Vicente", "Usulutan", 82);
+            grafo.AgregarArista("San Vicente", "Cabañas", 39);
+            grafo.AgregarArista("San Vicente", "La Paz", 48);
+            grafo.AgregarArista("San Vicente", "San Miguel", 97);
+            grafo.AgregarArista("Usulutan", "San Miguel", 77);
+            grafo.AgregarArista("San Miguel", "Morazan", 66);
+            grafo.AgregarArista("San Miguel", "La Union", 67);
+            grafo.AgregarArista("Morazan", "La Union", 54);
+        }
+
+        private void LlenarComboBoxes()
+        {
+            comboBoxInicio.Items.AddRange(grafo.Vertices.Keys.ToArray());
+            comboBoxFin.Items.AddRange(grafo.Vertices.Keys.ToArray());
+            comboBoxInicio.SelectedIndex = 0;
+            comboBoxFin.SelectedIndex = 0;
+        }
+
+
+        private void DibujarGrafo(List<string> recorrido)
+        {
+            Bitmap bitmap = new Bitmap(pictureBoxGrafo.Width, pictureBoxGrafo.Height);
+            Graphics g = Graphics.FromImage(bitmap);
+
+            var recorridoSet = new HashSet<string>(recorrido ?? new List<string>());
+
+            // Dibujar aristas
+            foreach (var vertice in grafo.Vertices.Values)
+            {
+                Point pos1 = ObtenerPosicion(vertice.Nombre);
+                foreach (var arista in vertice.Adyacencias)
+                {
+                    Point pos2 = ObtenerPosicion(arista.Destino.Nombre);
+                    bool esParteDelRecorrido = recorridoSet.Contains(vertice.Nombre) && recorridoSet.Contains(arista.Destino.Nombre);
+                    Pen pen = esParteDelRecorrido ? new Pen(Color.Red, 2) : Pens.Black;
+                    g.DrawLine(pen, pos1, pos2);
+                    g.DrawString(arista.Distancia.ToString(), new Font("Arial", 8), Brushes.Black, (pos1.X + pos2.X) / 2, (pos1.Y + pos2.Y) / 2);
+                }
+            }
+
+            // Dibujar vértices
+            foreach (var vertice in grafo.Vertices.Values)
+            {
+                Point pos = ObtenerPosicion(vertice.Nombre);
+                bool esParteDelRecorrido = recorridoSet.Contains(vertice.Nombre);
+                Brush brush = esParteDelRecorrido ? Brushes.Red : Brushes.LightBlue;
+                g.FillEllipse(brush, pos.X - 10, pos.Y - 10, 20, 20);
+                g.DrawEllipse(Pens.Black, pos.X - 10, pos.Y - 10, 20, 20);
+                g.DrawString(vertice.Nombre, new Font("Arial", 8), Brushes.Black, pos.X + 12, pos.Y);
+            }
+
+            pictureBoxGrafo.Image = bitmap;
+        }
+
+        private Point ObtenerPosicion(string nombre)
+        {
+            // Define posiciones fijas para cada vértice 
+            switch (nombre)
+            {
+                case "Ahuachapan": return new Point(60,400); 
+                case "Santa Ana": return new Point(120, 200); 
+                case "Sonsonate": return new Point(230, 450); 
+                case "Chalatenango": return new Point(300, 100); 
+                case "Cuscatlan": return new Point(500, 200); 
+                case "San Salvador": return new Point(400, 290);
+                case "Cabañas": return new Point(650, 100); 
+                case "San Vicente": return new Point(630, 250); 
+                case "La Paz": return new Point(570, 365); 
+                case "La Libertad": return new Point(330, 380); 
+                case "Usulutan": return new Point(720, 430);
+                case "San Miguel": return new Point(790, 290); 
+                case "Morazan": return new Point(940, 200); 
+                case "La Union": return new Point(990, 350); 
+                default: return new Point(0, 0);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Inicio inicio = new Inicio();
+            inicio.Show();
+           
+        }
+    }
+}
+
